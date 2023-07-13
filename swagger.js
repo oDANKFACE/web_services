a93b48007dc57c0aab45ee4faceea24ccdb84d40
oDANKFACE <darien.r.weller@gmail.com>
2023-07-13 15:51:06 -0600

    Almost forgot Swagger

--- swagger.js
+++ swagger.js
@@ -1,5 +1,4 @@
 const swaggerAutogen = require('swagger-autogen')();
-
 const doc = {
     info: {
         title: 'Project 1',
@@ -12,10 +11,4 @@ const doc = {
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-// generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
-
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./app.js');
-// });
